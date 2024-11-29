44d65c2e064a69362c2857c3cf00cd18f384b798
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2024-11-29 09:54:36 +0100

    start

---
+++ src/main/java/com/example/gladiolen_backend/GladiolenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gladiolen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GladiolenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GladiolenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gladiolen_backend/GladiolenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gladiolen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GladiolenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
