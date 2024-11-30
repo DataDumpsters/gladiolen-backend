1f5015d493fe8aa6cd522e850ae72e3e42b0bc8e
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-11-30 15:36:33 +0100

    post server working without backend

--- src/main/java/Project40/gladiolen_backend/config/SecurityConfig.java
+++ src/main/java/Project40/gladiolen_backend/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package Project40.gladiolen_backend.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -25,7 +25,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .csrf().disable();
 
         return http.build();
     }
