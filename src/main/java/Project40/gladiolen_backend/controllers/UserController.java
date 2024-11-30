04696f5506a7f35516bbffe5ac815720c95be740
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-11-30 13:10:59 +0100

    testing java security

--- src/main/java/Project40/gladiolen_backend/controllers/UserController.java
+++ src/main/java/Project40/gladiolen_backend/controllers/UserController.java
@@ -1,4 +1,13 @@
 package Project40.gladiolen_backend.controllers;
 
+import Project40.gladiolen_backend.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
 }
--- src/main/java/Project40/gladiolen_backend/services/UserService.java
+++ src/main/java/Project40/gladiolen_backend/services/UserService.java
@@ -1,4 +1,27 @@
 package Project40.gladiolen_backend.services;
 
+import Project40.gladiolen_backend.models.User;
+import Project40.gladiolen_backend.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        // Add some users
+        if (userRepository.count() <= 0) {
+            User user1 = new User();
+            user1.setFirstName("John");
+            user1.setLastName("Doe");
+            user1.setEmail("johndoe@test.com");
+            user1.setPassword("password");
+            userRepository.save(user1);
+        }
+    }
 }
