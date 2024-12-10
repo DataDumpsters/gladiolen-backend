af294899395aa51c4b4944c2fa5316ce94bc2b5d
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2024-12-10 11:43:25 +0100

    presignuprequestchange

--- src/main/java/Project40/gladiolen_backend/controllers/UserController.java
+++ src/main/java/Project40/gladiolen_backend/controllers/UserController.java
@@ -34,6 +34,13 @@ public class UserController {
         return userService.getDetails(jwtUtils.extractUserId(header));
     }
 
+    @GetMapping(value = "/user/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.OK)
+    @Operation(summary = "Returns all user account details")
+    public List<User> allUserDetailsRetrievalHandler() {
+        return userService.getAllUsers();
+    }
+
     @DeleteMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
     @Operation(summary = "Deletes a user account")
@@ -42,6 +49,50 @@ public class UserController {
             @RequestHeader(required = true, name = "Authorization") final String header) {
 
         return userService.deleteAccount(jwtUtils.extractUserId(header));
+    }
+
+    @PostMapping(value = "/admin/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Creates a new user account")
+    public ResponseEntity<?> createAccount(@RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping(value= "create-user", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Creates a new user account")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Creates a new user account based on role")
+    public ResponseEntity<?> createUserBasedOnRole(@RequestBody User user) {
+        if (user.getRole() == Role.Lid) {
+            userService.createUser(user);
+        } else {
+            userService.createAccount(user);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping(value = "/admin/user/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Updates a user account by ID")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User user) {
+        userService.updateUser(id, user);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/admin/user/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Deletes a user account by ID")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
     }
 
 //    private final UserService userService;
@@ -84,13 +135,13 @@ public class UserController {
 //        userService.deleteUser(id);
 //    }
 //
-//    @GetMapping("/roles")
-//    @ResponseStatus(HttpStatus.OK)
-//    public List<String> getRoles() {
-//        return Stream.of(Role.values())
-//                .map(Role::name)
-//                .collect(Collectors.toList());
-//    }
+    @GetMapping("/roles")
+    @ResponseStatus(HttpStatus.OK)
+    public List<String> getRoles() {
+        return Stream.of(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toList());
+    }
 //    Email validation check
 //@GetMapping("/check-email")
 //public boolean checkEmailExists(@RequestParam String email) {
--- src/main/java/Project40/gladiolen_backend/services/UserService.java
+++ src/main/java/Project40/gladiolen_backend/services/UserService.java
@@ -174,28 +174,28 @@ public class UserService {
                         .accessToken(jwtUtils.generateAccessToken(user)).build());
     }
 
-//    @Transactional
-//    public void createUser(User user) {
-//        if (userRepository.findByEmail(user.getEmail()) != null) {
-//            throw new RuntimeException("Er bestaat reeds een gebruiker met email: " + user.getEmail());
-//        }
-//        if (user.getTshirt() != null) {
-//            tshirtService.createTshirt(user.getTshirt());
-//        }
-//        User user1 = User.builder()
-//                .firstName(user.getFirstName())
-//                .lastName(user.getLastName())
-//                .phoneNumber(user.getPhoneNumber())
-//                .email(user.getEmail())
-//                .role(user.getRole())
-//                .registryNumber(user.getRegistryNumber())
-//                .password(passwordEncoder.encode(user.getPassword()))
-//                .union(user.getUnion())
-//                .tshirt(user.getTshirt())
-//                .shifts(user.getShifts())
-//                .build();
-//        userRepository.save(user1);
-//    }
+    @Transactional
+    public void createUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("Er bestaat reeds een gebruiker met email: " + user.getEmail());
+        }
+        if (user.getTshirt() != null) {
+            tshirtService.createTshirt(user.getTshirt());
+        }
+        User user1 = User.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .phoneNumber(user.getPhoneNumber())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .registryNumber(user.getRegistryNumber())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .union(user.getUnion())
+                .tshirt(user.getTshirt())
+                .shifts(user.getShifts())
+                .build();
+        userRepository.save(user1);
+    }
 
 //    public User getUserByEmail(String email) {
 //        return userRepository.findByEmail(email);
@@ -205,36 +205,36 @@ public class UserService {
 //        return userRepository.findById(id).orElse(null);
 //    }
 //
-//    public List<User> getAllUsers() {
-//        return userRepository.findAll();
-//    }
-//
-//    @Transactional
-//    public void updateUser(Long id, User user) {
-//        User user1 = userRepository.findById(id).orElse(null);
-//        if (user1 != null) {
-//            if (user1.getTshirt() != null) {
-//                tshirtService.updateTshirt(user1.getTshirt().getId(), user.getTshirt());
-//            }
-//            user1.setFirstName(user.getFirstName());
-//            user1.setLastName(user.getLastName());
-//            user1.setPhoneNumber(user.getPhoneNumber());
-//            user1.setEmail(user.getEmail());
-//            user1.setRole(user.getRole());
-//            user1.setRegistryNumber(user.getRegistryNumber());
-//            user1.setPassword(user.getPassword());
-//            user1.setUnion(user.getUnion());
-//            user1.setTshirt(user.getTshirt());
-//            user1.setShifts(user.getShifts());
-//            userRepository.save(user1);
-//        }
-//    }
-//
-//    public void deleteUser(Long id) {
-//        User user = userRepository.findById(id).orElse(null);
-//        if (user != null) {
-//            tshirtService.deleteTshirt(user.getTshirt().getId());
-//        }
-//        userRepository.deleteById(id);
-//    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void updateUser(Long id, User user) {
+        User user1 = userRepository.findById(id).orElse(null);
+        if (user1 != null) {
+            if (user1.getTshirt() != null) {
+                tshirtService.updateTshirt(user1.getTshirt().getId(), user.getTshirt());
+            }
+            user1.setFirstName(user.getFirstName());
+            user1.setLastName(user.getLastName());
+            user1.setPhoneNumber(user.getPhoneNumber());
+            user1.setEmail(user.getEmail());
+            user1.setRole(user.getRole());
+            user1.setRegistryNumber(user.getRegistryNumber());
+            user1.setPassword(user.getPassword());
+            user1.setUnion(user.getUnion());
+            user1.setTshirt(user.getTshirt());
+            user1.setShifts(user.getShifts());
+            userRepository.save(user1);
+        }
+    }
+
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            tshirtService.deleteTshirt(user.getTshirt().getId());
+        }
+        userRepository.deleteById(id);
+    }
 }
