6f8074cfb08fe11c71807e94fb87ae67ace3986f
NicolasVanDyck <r0878921@student.thomasmore.be>
2025-01-03 18:35:35 +0100

    overview ordered T-shirts

--- src/main/java/Project40/gladiolen_backend/services/TshirtService.java
+++ src/main/java/Project40/gladiolen_backend/services/TshirtService.java
@@ -1,10 +1,15 @@
 package Project40.gladiolen_backend.services;
 
+import Project40.gladiolen_backend.dto.TshirtCountDto;
 import Project40.gladiolen_backend.models.Tshirt;
+import Project40.gladiolen_backend.models.User;
 import Project40.gladiolen_backend.repositories.TshirtRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +31,21 @@ public class TshirtService {
     public Tshirt getTshirtById(Long id) {
         return tshirtRepository.findById(id).orElseThrow(() -> new RuntimeException("Tshirt not found"));
     }
+
+    public List<Tshirt> getAllTshirts() {
+        return tshirtRepository.findAll();
+    }
+
+    public List<TshirtCountDto> getTshirtCountsByRoleSexAndSize() {
+        return tshirtRepository.findTshirtCountsByRoleSexAndSize().stream()
+                .map(result -> new TshirtCountDto(
+                        result[0].toString(),
+                        result[1].toString(),
+                        result[2].toString(),
+                        ((Long) result[3]).intValue()))  // Convert Long to Integer
+                .collect(Collectors.toList());
+    }
+
 
     @Transactional
     public void updateTshirt(Long id, Tshirt tshirt) {
