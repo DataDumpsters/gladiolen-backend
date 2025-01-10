3a5630ea1cd2695361db16dd6605f1c462bc6650
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2025-01-10 10:16:07 +0100

    passwordfix

--- src/main/java/Project40/gladiolen_backend/services/UserService.java
+++ src/main/java/Project40/gladiolen_backend/services/UserService.java
@@ -311,7 +311,7 @@ public class UserService {
             user1.setEmail(user.getEmail());
             user1.setRole(user.getRole());
             user1.setRegistryNumber(user.getRegistryNumber());
-            user1.setPassword(user.getPassword());
+            user1.setPassword(passwordEncoder.encode(user.getPassword()));
             user1.setUnion(user.getUnion());
             user1.setShifts(user.getShifts());
             userRepository.save(user1);
